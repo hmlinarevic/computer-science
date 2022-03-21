73adbeefd4e7b53ffaf7fcd92ede3753781b2aba
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-03-21 20:38:50 +0100

    remove other versions

--- algorithms/sort/bubble-sort.js
+++ algorithms/sort/bubble-sort.js
@@ -1,41 +1,15 @@
-const items = require('./test-items');
+const numbers = [8, 7, 5, 4, 2, 4, 3, 2, 1, 0];
 
-/*
-	my version
-*/
+/**
+ *
+ * Bubble Sort
+ * Time Complexity --> O(n^2)
+ * Space Complexity
+ */
+
 const bubbleSort = (numbers) => {
-  let swap = true;
   let swapped;
-  let temp;
-  let iterations = 0;
 
-  while (swap) {
-    swapped = false;
-    iterations++;
-
-    for (let i = 0; i < numbers.length - 1; i++) {
-      if (numbers[i] > numbers[i + 1]) {
-        temp = numbers[i];
-        numbers[i] = numbers[i + 1];
-        numbers[i + 1] = temp;
-        swapped = true;
-      }
-    }
-    swap = swapped ? true : false;
-  }
-
-  console.log('total iterations:', iterations);
-};
-// const sortedItems = bubbleSort(items);
-// console.log(sortedItems);
-
-// ----------------------------------------------------------------------------
-
-/*
-	fem version
-*/
-const bubbleSortPro = (numbers) => {
-  let swapped;
   do {
     swapped = false;
     for (let i = 0; i < numbers.length; i++) {
@@ -49,36 +23,3 @@ const bubbleSortPro = (numbers) => {
   } while (swapped);
   return numbers;
 };
-
-const sortedItems = bubbleSortPro(items);
-console.log(sortedItems);
-
-// ----------------------------------------------------------------------------
-
-const numbers = [8, 7, 5, 4, 2, 4, 3, 2, 1, 0];
-
-/**
- *
- * Bubble Sort
- * Time Complexity --> O(n^2)
- */
-
-const bubble = (items) => {
-  let swap, element;
-
-  do {
-    swap = false;
-
-    for (let i = 0; i < items.length - 1; i++) {
-      element = items[i];
-      if (element > items[i + 1]) {
-        items[i] = items[i + 1];
-        items[i + 1] = element;
-        swap = true;
-      }
-    }
-  } while (swap);
-};
-
-bubble(numbers);
-console.log(numbers);
